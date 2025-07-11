a53434d4180b1ea4003b536b933e2603b701b150
cgm <501704164@qq.com>
2025-07-11 17:45:49 +0800

    update

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     # model = NIC("nica.yaml", task='caption')
     # model.train(data="./cfg/datasets/flickr8k.yaml", device='0', imgsz=256, epochs=100, batch=32)
 
-    model = NIC("./runs/caption/train/version_1/checkpoints/last.pt", task='caption')
+    model = NIC("./runs/caption/train/version_2/checkpoints/last.pt", task='caption')
     model.train(data="./cfg/datasets/flickr8k.yaml", device='0', imgsz=256, epochs=100, batch=32, resume=True)
